c87b82dd96cd964c573626badcbf3b75af40f707
Lucca Miranda <lucca.silva@quintoandar.com.br>
2022-07-11 09:48:11 -0300

    5

--- query/processing.py
+++ query/processing.py
@@ -6,43 +6,45 @@ from index.structure import Index, TermOccurrence
 from index.indexer import Cleaner
 
 class QueryRunner:
-	def __init__(self,ranking_model:RankingModel,index:Index, cleaner:Cleaner):
+	def __init__(self, ranking_model:RankingModel, index:Index, cleaner:Cleaner):
 		self.ranking_model = ranking_model
 		self.index = index
 		self.cleaner = cleaner
 
 
-	def get_relevance_per_query(self) -> Mapping[str,Set[int]]:
+	def get_relevance_per_query(self) -> Mapping[str, Set[int]]:
 		"""
 		Adiciona a lista de documentos relevantes para um determinada query (os documentos relevantes foram
 		fornecidos no ".dat" correspondente. Por ex, belo_horizonte.dat possui os documentos relevantes da consulta "Belo Horizonte"
 
 		"""
 		dic_relevance_docs = {}
-		for arquiv in ["belo_horizonte","irlanda","sao_paulo"]:
+		for arquiv in ["belo_horizonte", "sao_paulo", "irlanda"]:
 			with open(f"relevant_docs/{arquiv}.dat") as arq:
-				dic_relevance_docs[arquiv] = set(arq.readline().split(","))
+				dic_relevance_docs[arquiv] = set(arq.readline().strip().split(","))
 		return dic_relevance_docs
 
-	def count_topn_relevant(self,n:int,respostas:List[int],doc_relevantes:Set[int]) -> int:
+	def count_topn_relevant(self, n:int, respostas:List[int], doc_relevantes:Set[int]) -> int:
 		"""
 		Calcula a quantidade de documentos relevantes na top n posições da lista lstResposta que é a resposta a uma consulta
 		Considere que respostas já é a lista de respostas ordenadas por um método de processamento de consulta (BM25, Modelo vetorial).
 		Os documentos relevantes estão no parametro docRelevantes
 		"""
-		#print(f"Respostas: {respostas} doc_relevantes: {doc_relevantes}")
 		relevance_count = 0
-        
+		for doc in respostas:
+			if doc in doc_relevantes:
+				relevance_count += 1
+
 
 		return relevance_count
 
-	def compute_precision_recall(self, n:int, lst_docs:List[int],relevant_docs:Set[int]) -> (float,float):
-		
+	def compute_precision_recall(self, n:int, lst_docs:List[int], relevant_docs:Set[int]) -> (float, float):
 		precision = None
 		recall = None
 		return precision, recall
-    
-    def get_query_term_occurence(self, query:str) -> Mapping[str,TermOccurrence]:
+
+
+	def get_query_term_occurence(self, query:str) -> Mapping[str, TermOccurrence]:
 		"""
 			Preprocesse a consulta da mesma forma que foi preprocessado o texto do documento (use a classe Cleaner para isso).
 			E transforme a consulta em um dicionario em que a chave é o termo que ocorreu
@@ -52,9 +54,6 @@ class QueryRunner:
 		"""
 		#print(self.index)
 		map_term_occur = {}
-
-
-
 		return map_term_occur
 
 	def get_occurrence_list_per_term(self, terms:List) -> Mapping[str, List[TermOccurrence]]:
@@ -62,10 +61,8 @@ class QueryRunner:
 			Retorna dicionario a lista de ocorrencia no indice de cada termo passado como parametro.
 			Caso o termo nao exista, este termo possuirá uma lista vazia
 		"""
-
-
-
 		return dic_terms
+
 	def get_docs_term(self, query:str) -> List[int]:
 		"""
 			A partir do indice, retorna a lista de ids de documentos desta consulta
@@ -84,8 +81,8 @@ class QueryRunner:
 	@staticmethod
 	def runQuery(query:str, indice:Index, indice_pre_computado:IndexPreComputedVals , map_relevantes:Mapping[str,Set[int]]):
 		"""
-			Para um daterminada consulta `query` é extraído do indice `index` os documentos mais relevantes, considerando 
-			um modelo informado pelo usuário. O `indice_pre_computado` possui valores précalculados que auxiliarão na tarefa. 
+			Para um daterminada consulta `query` é extraído do indice `index` os documentos mais relevantes, considerando
+			um modelo informado pelo usuário. O `indice_pre_computado` possui valores précalculados que auxiliarão na tarefa.
 			Além disso, para algumas consultas, é impresso a precisão e revocação nos top 5, 10, 20 e 50. Essas consultas estão
 			Especificadas em `map_relevantes` em que a chave é a consulta e o valor é o conjunto de ids de documentos relevantes
 			para esta consulta.
@@ -116,7 +113,7 @@ class QueryRunner:
 				print("Precisao @{n}: {precisao}")
 				print("Recall @{n}: {revocacao}")
 
-		#imprima aas top 10 respostas
+	# 	#imprima aas top 10 respostas
 
 	@staticmethod
 	def main():
@@ -129,7 +126,7 @@ class QueryRunner:
 		#Instancie o IndicePreCompModelo para pr ecomputar os valores necessarios para a query
 		print("Precomputando valores atraves do indice...");
 		check_time = CheckTime()
-        
+
 
 
 
@@ -137,7 +134,7 @@ class QueryRunner:
 
 		#encontra os docs relevantes
 		map_relevance = None
-		
+
 		print("Fazendo query...")
 		#aquui, peça para o usuário uma query (voce pode deixar isso num while ou fazer um interface grafica se estiver bastante animado ;)
 		query = "São Paulo";
